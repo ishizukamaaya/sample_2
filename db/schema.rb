846af30c21b43a6364569a75c49e8e72d5705f30
EC2 Default User <maayajas@gmail.com>
2021-08-28 02:20:31 +0000

    finish

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_27_071041) do
+ActiveRecord::Schema.define(version: 2021_08_28_011009) do
 
   create_table "lists", force: :cascade do |t|
     t.string "title"
     t.string "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_id"
   end
 
 end
